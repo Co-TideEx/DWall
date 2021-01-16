d230f221f836fc1430a648eb3d053638d67b087b
DasonJ <dasj31@hotmail.com>
2021-01-16 00:13:51 -0500

    commit

--- DWall/yhweu8it9 hfnair.py
+++ DWall/yhweu8it9 hfnair.py
@@ -1,2 +1,22 @@
-from wallpaper import get_wallpaper, set_wallpaper , 0)
+import tkinter as tk
+
+# Creating a tkinter widget
+window = tk.Tk()
+
+# Add title
+window.title("Background Manager")
+
+frame = tk.Frame(window)
+frame2 = tk.Frame(window)
+frame.pack(side=tk.LEFT)
+
+tk.Button(frame, text = "Previous Background").pack()
+#tk.setImage()
+
+tk.Button(frame2, text = "Select Background").pack()
+#tk.setImage()
+
+
+# Start GUI and main loop
+window.mainloop()
 
