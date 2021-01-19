2a78bf4f10e35c4bd0410f93f77832125247d64c
DasonJ <dasj31@hotmail.com>
2021-01-19 11:49:56 -0500

    commit

--- DWall/yhweu8it9 hfnair.py
+++ DWall/yhweu8it9 hfnair.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 # Creating a tkinter widget
 window = tk.Tk()
@@ -6,15 +7,21 @@ window = tk.Tk()
 # Add title
 window.title("Background Manager")
 
-frame = tk.Frame(window)
+frame = tk.Frame(window, bg='black')
 frame2 = tk.Frame(window)
-frame.pack(side=tk.LEFT)
+frame.pack(side=tk.LEFT, expand=True)
+frame2.pack(side=tk.LEFT, expand=True)
 
-tk.Button(frame, text = "Previous Background").pack()
-#tk.setImage()
+tk.Button(frame, text = "Previous Background").grid(row=1, column=1)
 
-tk.Button(frame2, text = "Select Background").pack()
-#tk.setImage()
+load = Image.open("jupiter.jpg")
+widthz, heightz = load.size
+load = load.resize((int(widthz/2), int(heightz/2)), Image.ANTIALIAS)
+render = ImageTk.PhotoImage(load)
+img = tk.Label(window, image=render)
+img.pack(expand=True)
+
+tk.Button(frame2, text = "Select Background").grid(row=1, column=1)
 
 
 # Start GUI and main loop
