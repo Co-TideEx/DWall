b9a22d8cb5b644ea50618236fb22d8805ade7c48
DasonJ <dasj31@hotmail.com>
2021-03-09 07:53:53 -0500

    Created Stock.py

---
+++ DWall/Stock.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import datetime
+import matplotlib.pyplot as pltyy
+import pandas_datareader.data as web
+#pip install pandas, pandas-datareader, matplotlib, beautifulsoup4, scikit-learn
+
+
+start = datetime.datetime(2015, 1, 1)
+end = datetime.datetime.now()
+df = web.DataReader("TSLA", 'yahoo', start, end)
+
+stockInfo = Label(df.head)
+stockInfo.pack()
+
+window = Tk()
