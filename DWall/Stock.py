5e65068a12b7a75d634cf439f4d4bece213929bb
DasonJ <dasj31@hotmail.com>
2021-03-23 20:50:17 -0400

    Stock changes

--- DWall/Stock.py
+++ DWall/Stock.py
@@ -1,15 +1,36 @@
 from tkinter import *
 import datetime
-import matplotlib.pyplot as pltyy
+#import matplotlib.pyplot as plt
+#from matplotlib import style
+#import pandas as pd
 import pandas_datareader.data as web
-#pip install pandas, pandas-datareader, matplotlib, beautifulsoup4, scikit-learn
+# pip install pandas, pandas-datareader, matplotlib, beautifulsoup4, scikit-learn
+
+window = Tk()
+
+def submission():
+    print(content.get())
+
+Label(window, text="Start Date").grid(row=0)
+Label(window, text="End Date").grid(row=1)
+
+content = StringVar()
+e1 = Entry(window, textvariable=content).grid(row=0, column=1)
+e2 = Entry(window).grid(row=1, column=1)
+
+Button(window, text='Submit', command=submission).grid(row=3, column=1, sticky=W, pady=4)
 
 
-start = datetime.datetime(2015, 1, 1)
+
+start = datetime.datetime(2021, 3, 1)
 end = datetime.datetime.now()
 df = web.DataReader("TSLA", 'yahoo', start, end)
 
-stockInfo = Label(df.head)
-stockInfo.pack()
+#df.to_csv('tsla.csv')
 
-window = Tk()
+
+#stockInfo = Label(window, "shit")
+#stockInfo.pack()
+
+
+window.mainloop()
--- DWall/yhweu8it9 hfnair.py
+++ DWall/yhweu8it9 hfnair.py
@@ -1,34 +1,67 @@
 import tkinter as tk
+from tkinter import *
 from PIL import Image, ImageTk
+from tkinter import filedialog
+from wallpaper import get_wallpaper, set_wallpaper
+import ctypes
 
-# Creating a tkinter widget
-window = tk.Tk()
+def change_wall(): 
+    
+    try: 
+        set_wallpaper(str(path.get())) 
+        check = "success"
+  
+    except: 
 
-# Add title
-window.title("Background Manager")
-window.attributes('-alpha',0.5)
+        print(path.get())
+        check = "Wallpaper not found !"
+    result.set(check) 
+  
+  
+def browseFiles(): 
+    filename = filedialog.askopenfilename(initialdir="/", title="Select a File", filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*"))) 
+    path.set(filename) 
+      
+    label_file_explorer.configure(text="File Opened: "+filename)
+    image()
+    return filename
 
-frame = tk.Frame(window, bg='black')
-frame2 = tk.Frame(window)
-frame.pack(side=tk.LEFT, expand=True)
-frame2.pack(side=tk.LEFT, expand=True)
+window = tk.Tk() 
+window.configure(bg='light grey') 
 
-#Frame 1 widgets
-tk.Button(frame, text = "Previous Background").grid(row=1, column=1)
+result = StringVar() 
+path = StringVar() 
 
-load = Image.open("jupiter.jpg")
-widthz, heightz = load.size
-while widthz >= 600:
-    load = load.resize((int(widthz/2), int(heightz/2)), Image.ANTIALIAS)
-    widthz, heightz = load.size
-render = ImageTk.PhotoImage(load)
-img = tk.Label(frame, image=render)
-img.grid(row=3, column=1)
+window.title("Background Manager")  
+  
+label_file_explorer = Label(window, text="Select a image", width=100, fg="blue")
 
-#Frame 2 widgets
-tk.Button(frame2, text = "Select Background").grid(row=1, column=1)
+Label(window, text="Select image : ", bg="light grey").grid(row=0, sticky=W) 
+Label(window, text="Status :", bg="light grey").grid(row=3, sticky=W) 
+  
+  
+Label(window, text="", textvariable=result, 
+      bg="light grey").grid(row=3, column=1, sticky=W) 
+  
+b = Button(window, text="Open", command=browseFiles, bg="white") 
+b.grid(row=0, column=2, columnspan=2, rowspan=2, padx=5, pady=5,) 
+  
+label_file_explorer.grid(column=1, row=1) 
+  
+c = Button(window, text="Apply", command=change_wall, bg="white") 
+c.grid(row=2, column=2, columnspan=2, rowspan=2, padx=5, pady=5,) 
 
+def image():
+   load = Image.open(path.get())
+   widthz, heightz = load.size
+   while widthz >= 600:
+       load = load.resize((int(widthz/2), int(heightz/2)), Image.ANTIALIAS)
+       widthz, heightz = load.size
+   render = ImageTk.PhotoImage(load)
+   img = tk.Label(window, image=render)
+   img.grid(row=3, column=1)
 
-# Start GUI and main loop
 window.mainloop()
+
+
 
